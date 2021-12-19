c46783f0bf397bb8b286ce8e3991274e29c5ce93
JNewton <jnnewton@hiwaay.net>
2021-12-18 22:23:34 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="GreekLetters",
+    version="0.1.0",
+    description="Contains unicode for upper and lower case greek letters",
+    packages=["GreekLetters"],
+)
